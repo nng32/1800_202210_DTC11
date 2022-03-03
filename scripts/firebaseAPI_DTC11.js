2069e671b2ac24faa3b46b09903ee5b24c2b593b
nng32 <nng32@my.bcit.ca>
2022-03-02 20:45:38 -0800

    Finish login sequence

---
+++ scripts/firebaseAPI_DTC11.js
@@ -0,0 +1,32 @@
+// Import the functions you need from the SDKs you need
+
+// import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyAU7nwLM1r533w4ghvEIdfXic7QW9iGAVg",
+
+    authDomain: "dtc11-1800-202210.firebaseapp.com",
+
+    projectId: "dtc11-1800-202210",
+
+    storageBucket: "dtc11-1800-202210.appspot.com",
+
+    messagingSenderId: "589377196642",
+
+    appId: "1:589377196642:web:37d1ba3957e6441ecbf73b"
+
+};
+
+
+// Initialize Firebase
+
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
