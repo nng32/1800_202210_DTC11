6ebaf6ffd70b152a55241a46669d654eaddaa2fe
nng32 <nng32@my.bcit.ca>
2022-03-02 22:26:46 -0800

    Add events overview with Firestore

---
+++ scripts/populate-agenda.js
@@ -0,0 +1,29 @@
+function displayCards() {
+    let cardTemplate = document.getElementById("agenda-event-card");
+
+    // user is currently hardcoded!!
+    db.collection("users").doc("8WRf773RjVaETBZjvf6n97JVJOg2").collection("events").get().then(snap => {
+        var i = 1;
+        snap.forEach(doc => {
+            var name = doc.data().name;
+            var details = doc.data().details;
+            var timestart = doc.data().timestart;
+            var timeend = doc.data().timeend;
+            let newcard = cardTemplate.content.cloneNode(true);
+
+            newcard.querySelector('.card-title').innerHTML = name;
+            // newcard.querySelector('.card-subtitle').innerHTML = timestart + "-" + timeend;
+            newcard.querySelector('.card-text').innerHTML = details;
+
+            // give unique ids to all elements for future use
+            // newcard.querySelector('.card-title').setAttribute("id", "ctitle" + i);
+            // newcard.querySelector('.card-text').setAttribute("id", "ctext" + i);
+            // newcard.querySelector('.card-image').setAttribute("id", "cimage" + i);
+
+            document.getElementById("events-container").appendChild(newcard);
+            i++;
+        })
+    })
+}
+
+displayCards();
