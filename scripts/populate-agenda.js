b59e8da63b18473f80b53ee4c426641da8e5730e
nng32 <nng32@my.bcit.ca>
2022-03-03 21:57:18 -0800

    Add location text in overview card

--- scripts/populate-agenda.js
+++ scripts/populate-agenda.js
@@ -7,12 +7,13 @@ function displayCards() {
         snap.forEach(doc => {
             var name = doc.data().name;
             var details = doc.data().details;
+            var location = doc.data().location;
             var timestart = doc.data().timestart;
             var timeend = doc.data().timeend;
             let newcard = cardTemplate.content.cloneNode(true);
 
             newcard.querySelector('.card-title').innerHTML = name;
-            // newcard.querySelector('.card-subtitle').innerHTML = timestart + "-" + timeend;
+            newcard.querySelector('.card-location').innerHTML = location;
             newcard.querySelector('.card-text').innerHTML = details;
 
             // give unique ids to all elements for future use
