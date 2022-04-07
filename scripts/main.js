7c37076e081991f3f92fcf5a05411c0f09f273b1
nng32 <nng32@my.bcit.ca>
2022-04-07 11:35:11 -0700

    Add login message

--- scripts/main.js
+++ scripts/main.js
@@ -2,3 +2,18 @@ function storeEventData(id) {
     localStorage.setItem("eventID", id);
     console.log(`Data for ${id} has been stored.`)
 }
+
+function displayUserName() {
+    // check if user is signed in
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+            db.collection("users").doc(user.uid).get().then(userDoc => {
+                $('#notifier').html(`Welcome, ${userDoc.data().name}!`);
+                console.log(userDoc.data().name);
+            })
+        }
+        else {
+            alert("Failed to retrieve data. Please check to make sure you are signed in.");
+        }
+    })
+}
